5dd6fb030cfbcd69d15aa94e6d9f55ba4ff3bf39
لطفی نیا ، طراحی وب <E.Lotfi@exir.rayaneh>
2023-09-18 00:40:21 +0900

    update

--- Controllers/GatewayController.cs
+++ Controllers/GatewayController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using pylon.Models;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace gateway.Controllers
@@ -17,21 +19,8 @@ namespace gateway.Controllers
             _config = config;
         }
 
-
-        private bool CheckStoredProcedure(SqlConnection connection, string storedProcedureName)
-        {
-            using (SqlCommand checkCommand = new SqlCommand("SELECT COUNT(*) FROM sys.objects WHERE type = 'P' AND name = @storedProcedureName", connection))
-            {
-                checkCommand.Parameters.AddWithValue("@storedProcedureName", storedProcedureName);
-
-                int count = (int)checkCommand.ExecuteScalar();
-
-                return count > 0;
-            }
-        }
-
         [HttpPost("api/{*path}")]
-        public IActionResult Get(string path)
+        public IActionResult Get(string path, [FromBody] Dictionary<string, object>? inputModel)
         {
             List<Dictionary<string, object>> results = new List<Dictionary<string, object>>();
 
@@ -46,21 +35,37 @@ namespace gateway.Controllers
                     {
                         command.CommandType = CommandType.StoredProcedure;
 
-                        using (SqlDataReader reader = command.ExecuteReader())
+                        if(inputModel != null)
                         {
-                            while (reader.Read())
+                            foreach (var kvp in inputModel)
                             {
-                                Dictionary<string, object> result = new Dictionary<string, object>();
-
-                                for (int i = 0; i < reader.FieldCount; i++)
-                                {
-                                    string columnName = reader.GetName(i);
-                                    object columnValue = reader.GetValue(i);
-                                    result.Add(columnName, columnValue);
-                                }
-
-                                results.Add(result);
+                                command.Parameters.AddWithValue("@" + kvp.Key, kvp.Value.ToString());
                             }
+                        }
+
+                        try
+                        {
+                            using (SqlDataReader reader = command.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    Dictionary<string, object> result = new Dictionary<string, object>();
+
+                                    for (int i = 0; i < reader.FieldCount; i++)
+                                    {
+                                        string columnName = reader.GetName(i);
+                                        object columnValue = reader.GetValue(i);
+                                        result.Add(columnName, columnValue);
+                                    }
+
+                                    results.Add(result);
+                                }
+                            }
+                        }
+                        catch (System.Exception)
+                        {
+
+                            return StatusCode(405, "Params is wrong!");
                         }
                     }
                 } else
@@ -70,6 +75,66 @@ namespace gateway.Controllers
             }
 
             return Ok(results);
+        }
+
+        [HttpGet("api/list")]
+        public IActionResult GetStoredProcedures()
+        {
+            List<StoredProcedureInfo> procedureInfoList = new List<StoredProcedureInfo>();
+
+            using (SqlConnection connection = new SqlConnection(_config["ConnectionStrings:DefaultConnection"]))
+            {
+                connection.Open();
+
+                DataTable schemaTable = connection.GetSchema("Procedures");
+
+                foreach (DataRow row in schemaTable.Rows)
+                {
+                    string procedureName = row["ROUTINE_NAME"].ToString();
+                    List<SqlParameterInfo> parameters = GetStoredProcedureParameters(connection, procedureName);
+
+                    procedureInfoList.Add(new StoredProcedureInfo
+                    {
+                        Url =  procedureName.Replace("-", "_"),
+                        Model = parameters
+                    });
+                }
+            }
+
+            return Ok(procedureInfoList);
+        }
+
+        private List<SqlParameterInfo> GetStoredProcedureParameters(SqlConnection connection, string procedureName)
+        {
+            List<SqlParameterInfo> parameterInfoList = new List<SqlParameterInfo>();
+
+            DataTable schemaTable = connection.GetSchema("ProcedureParameters", new string[] { null, null, procedureName });
+
+            foreach (DataRow row in schemaTable.Rows)
+            {
+                string parameterName = row["PARAMETER_NAME"].ToString();
+                string parameterType = row["DATA_TYPE"].ToString();
+
+                parameterInfoList.Add(new SqlParameterInfo
+                {
+                    ParameterName = parameterName,
+                    ParameterType = parameterType
+                });
+            }
+
+            return parameterInfoList;
+        }
+
+        private bool CheckStoredProcedure(SqlConnection connection, string storedProcedureName)
+        {
+            using (SqlCommand checkCommand = new SqlCommand("SELECT COUNT(*) FROM sys.objects WHERE type = 'P' AND name = @storedProcedureName", connection))
+            {
+                checkCommand.Parameters.AddWithValue("@storedProcedureName", storedProcedureName);
+
+                int count = (int)checkCommand.ExecuteScalar();
+
+                return count > 0;
+            }
         }
 
     }
---
+++ Models/Gateway.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace pylon.Models
+{
+    public class StoredProcedureInfo
+    {
+        public string Url { get; set; }
+        public List<SqlParameterInfo> Model { get; set; }
+    }
+
+    public class SqlParameterInfo
+    {
+        public string ParameterName { get; set; }
+        public string ParameterType { get; set; }
+    }
+}
