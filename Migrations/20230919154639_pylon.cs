10b03d5ceda3c72502230a4622ab78a5ad1d9305
لطفی نیا ، طراحی وب <E.Lotfi@exir.rayaneh>
2023-09-20 00:50:53 +0900

    fix

---
+++ Migrations/20230919154639_pylon.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pylon.Migrations
+{
+    /// <inheritdoc />
+    public partial class pylon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
