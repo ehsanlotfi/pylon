0b3c3ce6de0dbed3e115f5080306ea551da379aa
لطفی نیا،مدیرتیم پشتیبانی سامانه ها <E.Lotfi@exir.rayaneh>
2024-09-14 12:26:04 +0330

    fix

--- pylonClient/src/app/app.component.ts
+++ pylonClient/src/app/app.component.ts
@@ -1,13 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+export const menuItems = [
+  {
+    "title": "Dashboard",
+    "icon": "bi bi-speedometer2",
+    "link": "/dashboard",
+    "children": []
+  },
+  {
+    "title": "Users",
+    "icon": "bi bi-people",
+    "link": "#",
+    "children": [
+      {
+        "title": "Add User",
+        "link": "/users/add"
+      },
+      {
+        "title": "Manage Users",
+        "link": "/users/manage"
+      }
+    ]
+  },
+  {
+    "title": "Settings",
+    "icon": "bi bi-gear",
+    "link": "#",
+    "children": [
+      {
+        "title": "General",
+        "link": "/settings/general"
+      },
+      {
+        "title": "Security",
+        "link": "/settings/security"
+      }
+    ]
+  }
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, NgbDropdownModule, CommonModule],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'pylonClient';
+export class AppComponent
+{
+  isLightMode = false;
+  menuItems = menuItems;
+
+  toggleDarkMode()
+  {
+    this.isLightMode = !this.isLightMode;
+  }
+
+  logout()
+  {
+    // Add logout logic here
+  }
 }
